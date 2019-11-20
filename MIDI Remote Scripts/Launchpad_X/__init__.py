fc7dd566fb2b969b1d5e6229a16d6b3ffed39cfd
aumhaa <aumhaa@gmail.com>
2019-11-19 22:13:48 -0800

    Live 10.1.4

--- MIDI Remote Scripts/Launchpad_X/__init__.py
+++ MIDI Remote Scripts/Launchpad_X/__init__.py
@@ -3,7 +3,7 @@ from .launchpad_x import Launchpad_X
 from ableton.v2.control_surface.capabilities import CONTROLLER_ID_KEY, NOTES_CC, PORTS_KEY, REMOTE, SCRIPT, SYNC, controller_id, inport, outport
 
 def get_capabilities():
-    return {CONTROLLER_ID_KEY: controller_id(vendor_id=4661, product_ids=[259], model_name=[u'Launchpad MK3']),
+    return {CONTROLLER_ID_KEY: controller_id(vendor_id=4661, product_ids=[259], model_name=[u'Launchpad X']),
      PORTS_KEY: [inport(props=[NOTES_CC, SCRIPT, REMOTE]), outport(props=[NOTES_CC,
                   SYNC,
                   SCRIPT,
